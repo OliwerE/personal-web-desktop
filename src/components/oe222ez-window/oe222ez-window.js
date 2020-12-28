d15256efeed361cf91c8c012545e3de588df7b08
Oliwer <oe222ez@student.lnu.se>
2020-12-28 18:44:40 +0100

    window component moves when user click inside the element once

--- src/components/oe222ez-window/oe222ez-window.js
+++ src/components/oe222ez-window/oe222ez-window.js
@@ -47,28 +47,55 @@ customElements.define('oe222ez-window',
     }
     
     moveWindow () {
-      this.window.onmousedown = this.mouseDownCoord
+      this.window.onmousedown = this.mouseDownCoord.bind(this)
     }
 
     mouseDownCoord (e) {
+      console.log(this.window)
       // funktionen används när musen flyttas
       const changeWindowPosition = (e) => {
         console.log('changes position!')
-        this.posX2 = e.pageX
-        this.posY2 = e.pageY
+        this.posX2 = e.clientX
+        this.posY2 = e.clientY
         console.log(this.posX2, this.posY2)
+        
+        // nya koordinater:
+
+        var newX = this.posX1 - this.posX2
+        var newY = this.posY1 - this.posY2
+
+        console.log('new x & y: ', newX, newY)
+
+        /*
+        // nolställer alla gamla positioner (fixa bättre lösning??)
+        this.posX1 = 0
+        this.posX2 = 0
+        this.posY1 = 0
+        this.posY2 = 0
+        */
+
+
+        // ändra div positionen:
+
+        //console.error(this.window) // löst anv: .bind(this)
+        
+        
+        this.window.style.top = (this.window.offsetTop - newY) + 'px'
+        this.window.style.left = (this.window.offsetLeft - newX) + 'px'
+      
       }
       // stoppa event onmouseup och onmousemove!
 
 
       // när fönstret flyttas:
+      this.posX1 = e.clientX
+      this.posY1 = e.clientY
+      
+      // hantera mouseup!
 
-      this.posX1 = e.pageX
-      this.posY1 = e.pageY
-      //document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
       console.log(this.posX1, this.posY1)
       document.onmousemove = changeWindowPosition
+
 
 
 
