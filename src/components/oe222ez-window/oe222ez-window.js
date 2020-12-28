5a4e58bb609495a7f23eb3930185fddecf5928e5
Oliwer <oe222ez@student.lnu.se>
2020-12-28 16:48:11 +0100

    window component logs first mousedown coordinate and detects everytime when the mouse moves.

--- src/components/oe222ez-window/oe222ez-window.js
+++ src/components/oe222ez-window/oe222ez-window.js
@@ -1,7 +1,7 @@
 /**
+ * Represents a window element.
  *
- *
- * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @author Oliwer Ellréus <oe222ez@student.lnu.se>
  * @version 1.0.0
  */
 
@@ -9,6 +9,7 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
 #window {
+  position: absolute;
   width: 300px;
   height: 200px;
   background-color: green;
@@ -31,7 +32,11 @@ customElements.define('oe222ez-window',
     }
 
     connectedCallback () {
-      console.log('A window component has started')
+      console.log('A window component added to dom!')
+      this.window = this.shadowRoot.querySelector('#window')
+      
+     this.moveWindow()
+
     }
 
     attributeChangedCallback (name, oldValue, newValue) {
@@ -40,5 +45,34 @@ customElements.define('oe222ez-window',
 
     disconnectedCallback () {
     }
+    
+    moveWindow () {
+      this.window.onmousedown = this.mouseDownCoord
+    }
+
+    mouseDownCoord (e) {
+
+      // stoppa event onmouseup och onmousemove!
+
+
+
+      // när fönstret flyttas:
+
+      this.posX1 = e.pageX
+      this.posY1 = e.pageY
+      //document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      console.log(this.posX1, this.posY1)
+      document.onmousemove = changeWindowPosition
+
+      function changeWindowPosition () {
+        console.log('changes position!')
+      }
+
+    }
+
+
+    
+
   }
 )
